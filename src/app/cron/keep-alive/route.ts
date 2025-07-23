f40c71fb23db4bcebf3465976748dc9f5e21030a
shrutiagarwal21 <khushiagarwal0204@gmail.com>
2025-07-24 02:29:25 +0530

    deleted unused corn

--- src/app/cron/keep-alive/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-// This will run only once when this API route is hit
-import "@/app/cron/taskNotification";
-
-export async function GET() {
-  return NextResponse.json({ status: "🟢 Task cron is active" });
-}
--- src/utils/sendNotification.ts
+++ src/utils/sendNotification.ts
@@ -1,16 +1,16 @@
-import { admin } from "../lib/firebaseAdmin";
+// import { admin } from "../lib/firebaseAdmin";
 
-export async function sendNotification(token: string, message: string) {
-  try {
-    await admin.messaging().send({
-      token,
-      notification: {
-        title: "Zencycle Reminder",
-        body: message,
-      },
-    });
-    console.log("✅ Notification sent");
-  } catch (err) {
-    console.error("❌ Error sending notification:", err);
-  }
-}
+// export async function sendNotification(token: string, message: string) {
+//   try {
+//     await admin.messaging().send({
+//       token,
+//       notification: {
+//         title: "Zencycle Reminder",
+//         body: message,
+//       },
+//     });
+//     console.log("✅ Notification sent");
+//   } catch (err) {
+//     console.error("❌ Error sending notification:", err);
+//   }
+// }
